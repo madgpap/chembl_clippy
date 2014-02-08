8ea6a3e8b7bcd4c9018f89cd243d2dc8119fc65e
mnowotka <mmmnow@gmail.com>
2014-02-08 15:05:58 +0100

    version bump

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ except ImportError:
 
 setup(
     name='chembl_clippy',
-    version='0.1.4',
+    version='0.1.5',
     scripts=['chembl_clippy/clippy_all.py'],
     author='George Papadatos',
     author_email='georgep@ebi.ac.uk',
